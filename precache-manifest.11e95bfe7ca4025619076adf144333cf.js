724310942b6dd3004a1cef71dfd4efd5ebfdb7b2
Joaquin mancheno <joaquinmancheno@Joaquins-MacBook-Pro.local>
2020-07-21 16:28:30 -0400

    Updates

---
+++ precache-manifest.11e95bfe7ca4025619076adf144333cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a1f8a50164f33a2c181159fffde111",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c97524d747504fea3471",
+    "url": "/monsters-rolodex/static/css/main.f87ab0a8.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c97524d747504fea3471",
+    "url": "/monsters-rolodex/static/js/main.27f12f41.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
